181fa2f8cc5d7a6e095ede430539de8b09ef1775
Karume-lab <danielkarume@outlook.com>
2025-02-27 19:22:47 +0300

    fix: :adhesive_bandage: update core components

---
+++ src/components/index.ts
@@ -0,0 +1,16 @@
+import EmblaCarousel from "embla-carousel";
+import Footer from "./core/Footer";
+import LinkAsLoadingButton from "./core/LinkAsLoadingButton";
+import LoadingButton from "./core/LoadingButton";
+import Header from "./core/Header";
+import SitePreviewIframe from "./core/SitePreviewIframe";
+
+export {
+  // core
+  EmblaCarousel,
+  LoadingButton,
+  Header,
+  Footer,
+  LinkAsLoadingButton,
+  SitePreviewIframe,
+};
